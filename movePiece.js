a05b99d9fd30dd383adbce6a466712bc5ba3b4d4
Thomas4897 <t.maynard4897@gmail.com>
2021-12-17 02:20:57 +0800

    Adding Piece Selector Restriction Base on Player Turn

--- movePiece.js
+++ movePiece.js
@@ -1,17 +1,6 @@
 let pieceType = "";
 let capturedPiece = "";
 let lastPosition = "";
-let playerNumber = 1;
-
-// let clickable = false;
-
-function playerTimerChange() {
-	if (playerNumber === 1) {
-		playerNumber = 2;
-	} else if (playerNumber === 2) {
-		playerNumber = 1;
-	}
-}
 
 function resetPieceAndPosition() {
 	pieceType = "";
@@ -22,45 +11,53 @@ function resetPieceAndPosition() {
 function movePiece() {
 	$(".piece").click(function () {
 		console.log(`New Move Player ${playerNumber}:`);
-
+		// $(this).css({
+		// 	background: "rgba(112, 128, 144, 0.63)",
+		// });
 		let pieceClass = $(this).attr("class");
 		let pieceClassArray = pieceClass.split(" ");
 
 		let notContainsPiece = pieceClassArray[1] === undefined;
 		let containsPiece = pieceClassArray[1] !== undefined;
 
-		// if (clickable === true) {
-		//! If the space that is clicked does not contain a piece then it will set its class to what is in localStorage
-		if (notContainsPiece && pieceType !== "") {
-			//?
-			$(this).addClass(pieceType);
-
-			if (lastPosition !== "") {
-				$(lastPosition).removeClass(pieceType);
-			}
-
-			playerTimerChange();
-			resetPieceAndPosition();
-		}
-
-		//*=================================
-		//! If the space that is clicked does not contain a piece then it will set its class to what is in localStorage
-		if (containsPiece && pieceType !== "") {
-			//?
-			capturedPiece = pieceClassArray[1];
-
-			if (capturedPiece[0] !== $(lastPosition).attr("class")[6]) {
-				$(this).removeClass(capturedPiece);
+		if (
+			(playerNumber === 1 && pieceType[0] === "w") ||
+			(playerNumber === 2 && pieceType[0] === "b") ||
+			pieceType === ""
+		) {
+			//! If the space that is clicked does not contain a piece then it will set its class to what is in localStorage
+			if (notContainsPiece && pieceType !== "") {
+				//?
 				$(this).addClass(pieceType);
-				$(lastPosition).removeClass(pieceType);
+
+				if (lastPosition !== "") {
+					$(lastPosition).removeClass(pieceType);
+				}
 
 				playerTimerChange();
 				resetPieceAndPosition();
 			}
-		} else if (containsPiece && pieceType === "") {
-			pieceType = pieceClassArray[1];
-			lastPosition = this;
+
+			//*=================================
+			//! If the space that is clicked does not contain a piece then it will set its class to what is in localStorage
+			if (containsPiece && pieceType !== "") {
+				//?
+				capturedPiece = pieceClassArray[1];
+
+				if (capturedPiece[0] !== $(lastPosition).attr("class")[6]) {
+					$(this).removeClass(capturedPiece);
+					$(this).addClass(pieceType);
+					$(lastPosition).removeClass(pieceType);
+
+					playerTimerChange();
+					resetPieceAndPosition();
+				}
+			} else if (containsPiece && pieceType === "") {
+				pieceType = pieceClassArray[1];
+				lastPosition = this;
+			}
+		} else {
+			pieceType = "";
 		}
-		// }
 	});
 }
--- timer.js
+++ timer.js
@@ -8,35 +8,32 @@ const interval = 1000;
 
 //? Player 1 Timer
 let timers = [5400, 5400];
+let playertimers = [player1Timer, player2Timer];
 let playerMinutes = Math.floor(timers[0] / 60);
 let playerSeconds = timers[0] % 60;
+let playerNumber = 1;
 
 player1Timer.text(`${playerMinutes}:0${playerSeconds}`);
 player2Timer.text(`${playerMinutes}:0${playerSeconds}`);
 
+function playerTimerChange() {
+	if (playerNumber === 1) {
+		playerNumber = 2;
+	} else if (playerNumber === 2) {
+		playerNumber = 1;
+	}
+}
+
 function setPlayersTimer(playerNumber) {
 	let playerMinutes = Math.floor(timers[playerNumber - 1] / 60);
 	let playerSeconds = timers[playerNumber - 1] % 60;
+	let pad = playerSeconds < 10 ? "0" : "";
 
-	if (playerNumber === 1) {
-		timers[playerNumber - 1]--;
+	timers[playerNumber - 1]--;
 
-		if (playerSeconds < 10) {
-			player1Timer.text(`${playerMinutes}:0${playerSeconds}`);
-		} else {
-			player1Timer.text(`${playerMinutes}:${playerSeconds}`);
-		}
-	}
-
-	if (playerNumber === 2) {
-		timers[playerNumber - 1]--;
-
-		if (playerSeconds < 10) {
-			player2Timer.text(`${playerMinutes}:0${playerSeconds}`);
-		} else {
-			player2Timer.text(`${playerMinutes}:${playerSeconds}`);
-		}
-	}
+	playertimers[playerNumber - 1].text(
+		`${playerMinutes}:${pad}${playerSeconds}`
+	);
 }
 
 function timer() {
